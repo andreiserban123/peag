eef7ee9df9d8cc39daffbc73506d41e6681861af
andreiserban123 <andyspeed2003@gmail.com>
2024-05-13 22:39:10 +0300

    s4

--- test2/s4.py
+++ test2/s4.py
@@ -1,164 +1,156 @@
-import numpy
+import numpy as np
 
-'''
-Functie de calculare a fitnessului
-'''
+# Datele problemei
+costuri = np.array([50, 70, 90, 60, 70, 100])
 
+capacitate_fabrica = [120, 140]
+necesar_depozite = [100, 60, 80]
 
-def fitness(indiv):
-    cost = 50 * indiv[0] + 70 * indiv[1] + 90 * indiv[2] + 60 * indiv[3] + 70 * indiv[4] + 100 * indiv[5]
-    return 1 / cost
+########################## FUNCTIE FITNESS + FEZABILITATE ########################
+def calculeaza_cost(X):
+    return np.sum(X * costuri)
 
-
-def generare_populatie_initiala(dim):
-    pop = []
-    for i in range(dim):
-        valid = False
-        while valid == False:
-            Xbp = numpy.random.randint(0, 101)
-            Xcp = 100 - Xbp
-
-            Xbpe = numpy.random.randint(0, min(120 - Xbp, 60) + 1)
-            Xcpe = 60 - Xbpe
-
-            if Xcp + Xcpe <= 140:
-                Xbc = numpy.random.randint(0, min(120 - Xbp - Xbpe, 80) + 1)
-                Xcc = 80 - Xbc
-
-                if Xcp + Xcpe + Xcc <= 140:
-                    valid = True
-                    indiv = [Xbp, Xbpe, Xbc, Xcp, Xcpe, Xcc]
-                    fit = fitness(indiv)
-                    indiv.append(fit)
-                    pop.append(indiv)
-    return pop
-
-
-def check_indiv(indiv):
-    if (indiv[0] + indiv[1] + indiv[2] > 120):
+def is_feasible(X):
+    # Verificăm constrângerile capacităților fabricilor
+    if X[0] + X[1] + X[2] > capacitate_fabrica[0]:  # București
         return False
-    if (indiv[3] + indiv[4] + indiv[5] > 140):
+    if X[3] + X[4] + X[5] > capacitate_fabrica[1]:  # Craiova
         return False
-    if (indiv[0] + indiv[3] != 100):
+    # Verificăm constrângerile necesarului depozitelor
+    if X[0] + X[3] != necesar_depozite[0]:  # Ploiești
         return False
-    if (indiv[1] + indiv[4] != 60):
+    if X[1] + X[4] != necesar_depozite[1]:  # Pitești
         return False
-    if (indiv[2] + indiv[5] != 80):
+    if X[2] + X[5] != necesar_depozite[2]:  # Cluj
         return False
     return True
 
+def fitness(X):
+    if not is_feasible(X):
+        return float('inf')
+    return calculeaza_cost(X)
 
-'''
-Selectia parintilor prin turnir
-'''
+########################## GENERAREA UNEI POPULATII ########################
+def gen_populatie(dim):
+    pop = []
+    for _ in range(dim):
+        X = np.zeros(6, dtype=int)
+        X[0] = np.random.randint(0, min(capacitate_fabrica[0], necesar_depozite[0]) + 1)
+        X[3] = necesar_depozite[0] - X[0]
 
+        X[1] = np.random.randint(0, min(capacitate_fabrica[0] - X[0], necesar_depozite[1]) + 1)
+        X[4] = necesar_depozite[1] - X[1]
 
-def turnir(pop, k):
-    pop_turnir = []
-    for i in range(k):
-        index = numpy.random.randint(0, len(pop))
-        pop_turnir.append(pop[index])
-    fitnesses = []
-    for i in range(k):
-        fitnesses.append(pop_turnir[i][-1])
-    index_castigator = numpy.argmax(fitnesses)
-    return pop_turnir[index_castigator]
+        X[2] = np.random.randint(0, min(capacitate_fabrica[0] - X[0] - X[1], necesar_depozite[2]) + 1)
+        X[5] = necesar_depozite[2] - X[2]
 
+        if is_feasible(X):
+            pop.append((X, fitness(X)))
+    return pop
 
-def selectia_parintilor(pop, k):
-    pop_parinti = []
+########################### OPERATOR MUTATIE #########################
+def mutatie(X):
+    i = np.random.randint(0, 6)
+    cantitate = np.random.randint(-5, 6)
+    X[i] = max(0, X[i] + cantitate)
+    return X
+
+def mutatie_populatie(pop, probabilitate_m):
+    pop_m = pop.copy()
     for i in range(len(pop)):
-        indiv_castigator = turnir(pop, k)
-        pop_parinti.append(indiv_castigator)
-    return pop_parinti
+        if np.random.uniform(0, 1) <= probabilitate_m:
+            X = pop[i][0].copy()
+            X = mutatie(X)
+            if is_feasible(X):
+                pop_m[i] = (X, fitness(X))
+    return pop_m
 
+########################## OPERATOR RECOMBINARE ########################
+def recombinare_uniforma(X1, X2):
+    X_new = X1.copy()
+    mask = np.random.randint(0, 2, size=6)
+    X_new[mask == 1] = X2[mask == 1]
+    return X_new
 
-'''
-Generearea copiilor
-'''
+def recombinare_populatie(pop, probabilitate_crossover):
+    copii = pop.copy()
+    for i in range(0, len(pop) - 1, 2):
+        if np.random.uniform(0, 1) <= probabilitate_crossover:
+            X1 = pop[i][0]
+            X2 = pop[i+1][0]
+            X_new1 = recombinare_uniforma(X1, X2)
+            X_new2 = recombinare_uniforma(X2, X1)
+            if is_feasible(X_new1):
+                copii[i] = (X_new1, fitness(X_new1))
+            if is_feasible(X_new2):
+                copii[i+1] = (X_new2, fitness(X_new2))
+    return copii
 
+########################## SELECTIE PARENTI SUS ########################
+def fps(fitnessuri):
+    fitnessuri = np.array(fitnessuri)
+    suma = np.sum(fitnessuri)
+    fps = fitnessuri / suma
+    qfps = np.cumsum(fps)
+    return qfps
 
-def recombinare_parinti(pop):
-    pop_copii = pop.copy()
-    for i in range(0, len(pop), 2):
-        if i + 1 < len(pop):
-            indiv1 = pop[i][:-1]
-            indiv2 = pop[i + 1][:-1]
-            index = numpy.random.randint(0, len(pop[i]))
-            copil1 = indiv1[:index] + indiv2[index:]
-            copil2 = indiv2[:index] + indiv1[index:]
-            copil1.append(fitness(copil1))
-            copil2.append(fitness(copil2))
-            pop_copii[i] = copil1.copy()
-            pop_copii[i + 1] = copil2.copy()
-    return pop_copii
+def ruleta(pop_initiala):
+    dim = len(pop_initiala)
+    fitnessuri = [individ[1] for individ in pop_initiala]
+    qfps = fps(fitnessuri)
+    parinti = pop_initiala.copy()
+    for i in range(dim):
+        r = np.random.uniform(0, 1)
+        pozitie = np.where(qfps >= r)[0][0]
+        parinti[i] = pop_initiala[pozitie]
+    return parinti
 
+########################## ELITISM ########################
+def elitism(pop_curenta, pop_mutanta):
+    dim = len(pop_curenta)
+    pop_urmatoare = pop_mutanta.copy()
+    max_curent = min([individ[1] for individ in pop_curenta])
+    max_mutant = min([individ[1] for individ in pop_mutanta])
+    if max_curent < max_mutant:
+        poz = [i for i, individ in enumerate(pop_curenta) if individ[1] == max_curent][0]
+        ir = np.random.randint(dim)
+        pop_urmatoare[ir] = pop_curenta[poz]
+    return pop_urmatoare
 
-'''
-Mutatie
-'''
+##################################### APLICARE GA FINAL ###################################
+def GA(dim, NMAX, pc, pm):
+    pop_initiala = gen_populatie(dim)
+    istoric_v = [min([individ[1] for individ in pop_initiala])]
+    it = 0
+    gata = False
+    nrm = 1
 
-
-def mutatie(pop, prob_mut):
-    pop_mutanti = pop.copy()
-    for indiv in pop:
-        indiv_nou = indiv[:-1]
-        for index in range(3):
-            if numpy.random.uniform(0, 1) < prob_mut:
-                val = numpy.random.randint(-1, 2)
-                nou_val = indiv_nou[index] + val
-                nou_val2 = indiv_nou[index + 3] - val
-                if 0 <= nou_val <= 120 and 0 <= nou_val2 <= 140:
-                    indiv_nou[index] = nou_val
-                    indiv_nou[index + 3] = nou_val2
-        if check_indiv(indiv_nou):
-            indiv_nou.append(fitness(indiv_nou))
-            pop_mutanti.append(indiv_nou)
+    while it < NMAX and not gata:
+        parinti = ruleta(pop_initiala)
+        pop_copii = recombinare_populatie(parinti, pc)
+        pop_copii_mutanti = mutatie_populatie(pop_copii, pm)
+        pop_urmatoare = elitism(pop_initiala, pop_copii_mutanti)
+        minim = min([individ[1] for individ in pop_urmatoare])
+        maxim = max([individ[1] for individ in pop_urmatoare])
+        if minim == istoric_v[it]:
+            nrm += 1
         else:
-            pop_mutanti.append(indiv)
-    return pop_mutanti
-
-
-'''
-Functie de implementare a algoritmului
-'''
-MAX_GENERATIONS = 25
-NO_IMPROVEMENT_LIMIT = 5
-
-
-def algoritm(dim, k):
-    populatia = generare_populatie_initiala(dim)
-    best_fitness = 0
-    best_individual = None
-    generatii_fara_imbunatatire = 0
-    current_generation = 0
-
-    while current_generation < MAX_GENERATIONS and generatii_fara_imbunatatire < NO_IMPROVEMENT_LIMIT:
-        populatia_parinti = selectia_parintilor(populatia, k)
-        populatia_copii = recombinare_parinti(populatia_parinti)
-        populatia_mutanta = mutatie(populatia_copii, 0.2)
-
-        populatia = populatia_mutanta
-        current_best = max(populatia, key=lambda indiv: indiv[-1])
-
-        if current_best[-1] > best_fitness:
-            best_fitness = current_best[-1]
-            best_individual = current_best
-            generatii_fara_imbunatatire = 0
+            nrm = 0
+        if minim == maxim or nrm == int(NMAX / 4):
+            gata = True
         else:
-            generatii_fara_imbunatatire += 1
+            it += 1
+        istoric_v.append(minim)
+        pop_initiala = pop_urmatoare.copy()
 
-        current_generation += 1
+    poz_max = [i for i, individ in enumerate(pop_urmatoare) if individ[1] == minim][0]
+    individ_max_gene = pop_urmatoare[poz_max][0]
+    individ_max_fitness = minim
 
-        print(f"Generația {current_generation}: Cost: {1 / best_fitness}")
+    return individ_max_gene, individ_max_fitness
 
-    print("Cel mai bun individ final:")
-    print(best_individual)
-    print("Cost:")
-    print(1 / best_fitness)
-
-
-'''
-Apelarea algortimului
-'''
-algoritm(5, 3)
+######################################## RULARE ##############################################
+if __name__ == "__main__":
+    individ_max_gene, individ_max_fitness = GA(100, 200, 0.8, 0.2)
+    print("Gene: ", individ_max_gene)
+    print("Fitness: ", individ_max_fitness)
